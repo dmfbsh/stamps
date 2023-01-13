fa3ab142d69a14b28344949f921c49fc5c2dc41c
dmfbsh <51413860+dmfbsh@users.noreply.github.com>
2023-01-13 17:48:42 +0000

    Updates

---
+++ _build.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+CD C:\Users\David\Documents\OneDrive\Documents\My Documents\GitHub\stamps
+
+jekyll build --verbose --config _config.yml
+
+PAUSE
--- _build2.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-CD C:\Users\David\Documents\OneDrive\Documents\My Documents\GitHub\dmfbsh.github.io
-
-jekyll build --verbose --config _config2.yml
-
-PAUSE
